e6017167ec22b77baf24d204d2af75611758138e
I4NIS <ianis11l.leblay@gmail.com>
2024-03-19 22:23:40 +0100

    adding of the new files

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,9 @@
 
 <template>
   <h1 class="text-3xl font-bold underline text-green-500">
-    Hello world!
+    Hello world !
   </h1>
+  <router-view></router-view>
 </template>
 
 <style scoped>
---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from '@/views/LoginView.vue'
+import SignUpView from '@/views/SignUpView.vue'
+import MatchView from '@/views/MatchView.vue'
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'matches',
+            component: MatchView
+        },
+        {
+            path: '/signup',
+            name: 'signup',
+            component: SignUpView
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: LoginView
+        }
+    ]
+})
+
+export default router
