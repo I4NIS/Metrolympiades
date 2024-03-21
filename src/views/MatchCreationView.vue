846777f4db16eb97d063e64913397fbf05da054a
I4NIS <113393648+I4NIS@users.noreply.github.com>
2024-03-21 13:33:58 +0100

    Setup

---
+++ src/views/MatchCreationView.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+<h1>Match création</h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/TeamSettingsView.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+<h1>Team Settings</h1>
+</template>
+
+<style scoped>
+
+</style>
