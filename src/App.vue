94487614669c8c20a09b9c7bddc615bb8c301515
I4NIS <113393648+I4NIS@users.noreply.github.com>
2024-03-21 15:25:11 +0100

    fix NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <script setup>
-import {ref, watchEffect} from 'vue';
+import { ref, watchEffect } from 'vue';
+import { useRoute } from 'vue-router';
 import AppNavBar from "@/components/AppNavBar.vue";
 
 const showNavBar = ref(true);
+const route = useRoute();
 
-
-
-
+watchEffect(() => {
+  const currentPath = route.path;
+  showNavBar.value = !(currentPath === '/login' || currentPath === '/signup');
+});
 </script>
+
 <template>
   <div>
     <AppNavBar v-if="showNavBar"/>
     <router-view/>
   </div>
 </template>
-
-
 
 <style scoped>
 </style>
